deb1a8bc7389b94eae45aca5acdcce8e69761519
Matheus Goulart <matheus.goulart@ozmap.com.br>
2021-09-16 11:03:53 -0300

    Option buttons color and "dark" property now come from the settings.js file.

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,5 +1,11 @@
 <template>
   <v-main>
+    <v-btn to="./" class="mt-1 mb-2" :color="settings.style.optionButtons.color" :dark="settings.style.optionButtons.dark" >
+      <v-icon>
+        mdi-arrow-left
+      </v-icon>
+      Main menu
+    </v-btn>
     <v-app-bar height="100" class="ma-10 d-flex justify-center align-center text-h3 text-center">
         {{currentPage.title}}
     </v-app-bar>
@@ -9,7 +15,7 @@
      
     </v-card>
     <div width="80%" class="d-flex justify-center align-start flex-wrap text-center">
-      <v-btn v-for="option in currentPage.options" :key="option.name" @click="goToPage(option.goTo)" class="ma-5" color="green" dark>
+      <v-btn v-for="option in currentPage.options" :key="option.name" @click="goToPage(option.goTo)" class="ma-5" :color="settings.style.optionButtons.color" :dark="settings.style.optionButtons.dark">
         {{option.name}}
       </v-btn>
     </div>
@@ -18,11 +24,13 @@
 
 <script>
 import { story } from "../stories/story";
+import { settings } from "../settings/settings";
 export default {
   data() {
     return {
       story: story,
       currentPage: story.page1,
+      settings: settings,
     };
   },
   methods: {
