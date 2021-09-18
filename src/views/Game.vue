42a9a0410c3702ba55c33068c6689840871d26ad
Matheus Goulart <matheus.goulart@ozmap.com.br>
2021-09-17 21:45:52 -0300

    Update Game.vue

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -47,4 +47,11 @@ export default {
 </script>
 
 <style scoped>
+.v-main {
+  background: no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
 </style>
