d159412a9a03c2d3f4eb0bf4d9da43350241be24
Matheus Goulart <matheus.goulart@ozmap.com.br>
2021-09-19 22:12:24 -0300

    (Slightly) Even better animations!

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>
-    <title-bar/>
-    <div class="d-flex justify-center">
-    <v-btn to="./game" class="ma-2" color="green" dark x-large>Play</v-btn>
+  <transition-group mode="in-out" name="fade-in">
+    <div :key="outTitleBar">
+      <title-bar />
+      <div class="d-flex justify-center">
+        <v-btn @click="play" class="ma-2" color="green" dark x-large>Play</v-btn>
+      </div>
     </div>
-  </div>
+  </transition-group>
 </template>
 
 <script>
@@ -16,10 +18,31 @@ export default {
   data() {
     return {
       settings: settings,
+      outTitleBar: false,
     };
+  },
+  methods: {
+    play() {
+      this.outTitleBar = true;
+      setTimeout(() => {
+        this.$router.push("/game");
+      }, 450);
+    },
   },
 };
 </script>
 
-<style>
+<style scoped>
+.fade-in-enter-active {
+  transition: opacity 300ms ease-in-out;
+  transition-delay: 1s;
+}
+
+.fade-in-leave-active {
+  transition: opacity 300ms ease-in-out;
+}
+.fade-in-enter,
+.fade-in-leave-to {
+  opacity: 0;
+}
 </style>
